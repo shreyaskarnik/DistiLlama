94815bded579e1682a4c621ca5fafc1939b72865
Shreyas Karnik <karnik.shreyas@gmail.com>
2024-03-03 17:52:52 -0800

    fix

--- src/pages/sidePanel/Instructions.tsx
+++ src/pages/sidePanel/Instructions.tsx
@@ -3,7 +3,10 @@ export default function Instructions() {
   const markdown = `
   # Ollama Not Running
   - Download Ollama from [here](https://ollama.ai)
-  - Make sure you set OLLAMA_ORIGINS=* for the Ollama environment by following instructions [here](https://github.com/ollama/ollama/blob/main/docs/faq.md#how-do-i-configure-ollama-server)
+   - To start the server, run the following command:
+    \`\`\`bash
+    OLLAMA_ORIGINS=* OLLAMA_HOST=127.0.0.1:11435 ollama serve
+    \`\`\`
   - Download models from [here](https://ollama.ai/library)
   `;
 
--- src/pages/utils/constants.ts
+++ src/pages/utils/constants.ts
@@ -1,1 +1,1 @@
-export const OLLAMA_BASE_URL = 'http://localhost:11434';
+export const OLLAMA_BASE_URL = 'http://localhost:11435';
